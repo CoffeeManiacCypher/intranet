2a0b11ddbfe90071797fadb0cb26b9651d54817a
CoffeeManiacCypher <109939128+CoffeeManiacCypher@users.noreply.github.com>
2024-12-16 02:58:56 -0300

    Actualizar el AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Support\Facades\Schema; // Importa la clase Schema
+use Illuminate\Support\Facades\URL;    // Importa la clase URL
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Aquí puedes registrar servicios adicionales si es necesario
     }
 
     /**
@@ -19,9 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Forzar HTTPS en producción
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        // Solución a problemas con MySQL y claves foráneas en Laravel
         Schema::defaultStringLength(191);
     }
 }
