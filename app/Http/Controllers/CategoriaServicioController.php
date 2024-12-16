76b53836fb9b3b99145ab8980a0a19448ded635b
CoffeeManiacCypher <109939128+CoffeeManiacCypher@users.noreply.github.com>
2024-12-16 03:32:59 -0300

    Add files via upload

--- app/Http/Controllers/CategoriaServicioController.php
+++ app/Http/Controllers/CategoriaServicioController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Http\Controllers;
@@ -35,42 +34,4 @@ class CategoriaServicioController extends Controller
 
         return response()->json(['message' => 'Categoría eliminada exitosamente.']);
     }
-=======
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\CategoriaServicio;
-use Illuminate\Http\Request;
-
-class CategoriaServicioController extends Controller
-{
-    public function index(Request $request)
-    {
-        $perPage = $request->get('per_page', 10);
-        $categorias = CategoriaServicio::paginate($perPage);
-
-        return view('categoriaservicio.index', compact('categorias'));
-    }
-
-    public function store(Request $request)
-    {
-        $validatedData = $request->validate([
-            'nombre' => 'required|max:255',
-            'descripcion' => 'nullable|string',
-        ]);
-
-        CategoriaServicio::create($validatedData);
-
-        return response()->json(['message' => 'Categoría creada exitosamente.'], 201);
-    }
-
-    public function destroy($id)
-    {
-        $categoria = CategoriaServicio::findOrFail($id);
-        $categoria->delete();
-
-        return response()->json(['message' => 'Categoría eliminada exitosamente.']);
-    }
->>>>>>> f54984645166dd5ed8a43c1a794037308fcf7b95
 }
--- app/Http/Controllers/ValoracionesController.php
+++ app/Http/Controllers/ValoracionesController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Http\Controllers;
@@ -13,19 +12,3 @@ class ValoracionesController extends Controller
         return view('valoraciones.index');
     }
 }
-=======
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class ValoracionesController extends Controller
-{
-    public function index()
-    {
-        return view('valoraciones.index');
-    }
-}
->>>>>>> f54984645166dd5ed8a43c1a794037308fcf7b95
